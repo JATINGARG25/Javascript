0067e3fe8cab785382a1db75c281c1b58986dc93
JATINGARG25 <gargjatin519@gmail.com>
2024-03-16 16:35:24 +0530

    learned operators

---
+++ Operators and Conditional Statements/operators.js
@@ -0,0 +1,32 @@
+// Arithmetic operators
+
+let a = 5;
+let b = 2;
+
+console.log("a + b =",a+b) // same for -, *, /, modulus, exponentiation
+
+// Uniary operators -- Increment and Decrement
+
+let c = 5;
+
+// console.log("c =",c++); // gives 5
+console.log("c =",++c); // gives 6 -- same in decrement
+
+// Assignment operators
+
+d = 5;
+d += 4
+console.log("d =",d)  // same for -=, *=, %=, **=
+
+// Comparison operator
+
+e = 5;
+f = 5;
+
+console.log(e==f) // same for !=, >, >=, <, <=
+
+// Logical operator
+
+// && -- AND , || -- OR , ! -- NOT
+
+
