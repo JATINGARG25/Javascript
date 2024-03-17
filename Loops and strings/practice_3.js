5fc8f40d1dfb96b430b81283ad3b3a470fee4072
JATINGARG25 <gargjatin519@gmail.com>
2024-03-17 12:51:00 +0530

    strings done

---
+++ Loops and strings/practice_3.js
@@ -0,0 +1,5 @@
+let fullName = prompt("Enter your Full Name:");
+
+let userName = "@"+fullName+fullName.length;
+console.log(userName);
+
---
+++ Loops and strings/strings.js
@@ -0,0 +1,9 @@
+// # string in js 
+
+let str = "Jatin Garg";
+
+// Template literal
+
+let sentence = `This is template literal`;
+
+let exp = `Sum of a+b is ${1+6}.`
